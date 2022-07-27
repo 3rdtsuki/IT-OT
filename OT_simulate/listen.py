757342b747da047d4445e3269ec9bbbdd1636dfd
Mikazuki <44475022+3rdtsuki@users.noreply.github.com>
2022-07-27 12:24:08 +0800

    Delete listen.py

--- OT_simulate/listen.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-cnt=0
-def monitor(packet):
-    global cnt
-    cnt+=1
-    print(cnt)
-packet = sniff(prn=monitor,filter="tcp",store=1,timeout=180)
-wrpcap(r'output.pcap', packet)
